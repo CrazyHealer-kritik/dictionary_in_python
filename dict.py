b5c2e6a32db9d7958f55a46574045e051e4fbc23
kritik sah <57076429+CrazyHealer-kritik@users.noreply.github.com>
2020-05-22 01:22:11 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 00:33:58 2020
+
+@author: kritik
+"""
+
+import json#importing json library
+
+re = "y"
+while re == "y" or re == "yes":
+    data = json.load(open("data.json"))#loading and opening data.json file inside this python file
+
+    def meaningsOf(word):#function to find word in data.json
+        if word in data:
+            return data[word]
+        
+    
+    word = input("enter the word :\t").lower()#user input for searching meanings
+    result = meaningsOf(word)
+    if result == None:#if there is no matching words in data.json
+        print("Sorry no result found for this word\n please try something different")
+    else:
+        print(result)
+        
+    re = input("re-find (yes/no or y/n) :\t").lower()
+
